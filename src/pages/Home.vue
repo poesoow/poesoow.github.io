812a4f42a7f581d176dd21482ab7f7b97fcb0f80
peosoow <ws71144712@daum.net>
2023-04-04 14:11:14 +0900

    [update] home 언어 변경되도록 함

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,10 @@
 <template>
-  
   <div class="w-full">
     <div class="max-w-7xl mx-auto pt-10 lg:pt-40">
       <div class="flex flex-wrap items-center">
         <div class="basis-full md:basis-2/4 text-center py-3">
-          <p class="text-green-600 hover:text-green-500 text-2xl lg:text-4xl font-medium lg:font-semibold mb-5 lg:leading-normal leading-snug dark:[text-#d9d9d9]">
-            안녕하세요 ... <br>
-            프론트엔드 개발자 <br>
-            홍길동 입니다.
+          <p v-html="langList.MainTxt[0]" class="text-green-600 hover:text-green-500 text-2xl lg:text-4xl font-medium lg:font-semibold mb-5 lg:leading-normal leading-snug dark:[text-#d9d9d9]">
+            
           </p>
         </div>
         <div class="basis-full md:basis-2/4 text-center">
