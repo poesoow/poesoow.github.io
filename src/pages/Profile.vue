c5b95843ad9695a1a489422f31fe4ff6fc8a9832
peosoow <ws71144712@daum.net>
2023-05-08 18:17:06 +0900

    프로필 작업 중

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,32 +1,34 @@
+<!-- eslint-disable vue/no-parsing-error -->
 <template>
   <div class="w-full pt-12 mt-6 px-[2%] pb-8 ">
-    <!-- 프롭스로 컴포넌트 활용한다고 수정 주석 처리
-      <div class="max-w-7xl p-5 mx-auto bg-white rounded-md border darkMode">
-      <h3 class="relative after:absolute mx-auto w-max after:w-[100%] after:h-[2px] after:bg-black after:dark:bg-[#d9d9d9] after:-bottom-1 after:left-2/4 after:-translate-x-2/4 tracking-widest text-center text-xl sm:text-2xl lg:text-3xl">프로필</h3>
-    </div> -->
     <Title title="프로필" />
     <div class="max-w-7xl mx-auto">
       <div class="h-auto flex overflow-hidden mt-8 flex-wrap justify-between">
         <div class="basis-full md:basis-[30%] py-12 px-4 bg-white rounded-md darkMode text-center border">
           <img src="https://via.placeholder.com/300" alt="사진" class="rounded-full block mx-auto">
           <ul>
-            <li class="pl-5 box-border pt-6">홍길동 / Hong Gil Dong</li>
-            <li class="pl-5 box-border pt-6">1920.01.01</li>
-            <li class="pl-5 box-border pt-6"><a href="mailto:aaa@naver.com">aaa@naver.com</a></li>
+            <li class="pl-5 box-border pt-6">송우섭 / Song Woo Seop</li>
+            <li class="pl-5 box-border pt-6">1995.02.09</li>
+            <li class="pl-5 box-border pt-6"><a href="mailto:ws71144612@daum.net">ws71144612@daum.net</a></li>
           </ul>
-          <span class="inline-block h-8 mt-2 mr-1 text-sm border bg-white rounded-2xl align-top leading-[19px] box-border tracking-tighter pt-1.5 px-3 pb-2 shadow-md darkMode">#성실하게</span>
-          <span class="inline-block h-8 mt-2 mr-1 text-sm border bg-white rounded-2xl align-top leading-[19px] box-border tracking-tighter pt-1.5 px-3 pb-2 shadow-md darkMode">#꼼꼼하게</span>
-          <span class="inline-block h-8 mt-2 mr-1 text-sm border bg-white rounded-2xl align-top leading-[19px] box-border tracking-tighter pt-1.5 px-3 pb-2 shadow-md darkMode">#이렇게</span>
         </div>
         <div class="basis-full md:basis-[67.5%] mt-8 md:mt-0">
 
 
           <div v-for="e in Content" :key="e" class="bg-white rounded-md border darkMode p-5 mb-8 last:mb-0">
             <h3 class="text-base md:text-xl mb-4 ">◆ {{ e.title }}</h3>
-            <!-- for문안에 if문으로 원하는 내용만 나오도록 하기 -->
-            <p v-if="e.desc" class="mb-2 last:mb-0 text-sm md:text-base"><span class="font-bold">{{ e.desc }}</span>{{ e.desc2 }}</p>
-            <!-- for문안에 같은구조로 만들고 for문으로 한번도 돌리기 -->
-            <p v-for="(el, index) in e.date" :key="el" class="mb-2 last:mb-0 text-sm md:text-base">{{ el }} : {{ e.dateDesc[index] }}</p>
+            <div class="mb-2 last:mb-0 text-sm md:text-base font-light">
+              <p class="mb-2" v-for="mind in e.mind" :key="mind">
+                <span class="font-bold">{{ mind.desc }}</span>
+                <div>{{ mind.desc2 }}</div>
+              </p>
+            </div>
+            <div class="mb-2 last:mb-0 text-sm md:text-base font-light">
+              <p v-for="(el, index) in e.date" :key="el" class="mb-2">{{ el }} : {{ e.dateDesc[index] }}</p>
+            </div>
+            <p class="flex flex-wrap mb-2 last:mb-0 text-sm md:text-base">
+              <span v-for="(el) in e.like" :key="el"  class="h-8 mt-2 mr-1 text-sm border bg-white rounded-2xl align-top leading-[19px] box-border tracking-tighter pt-1.5 px-3 pb-2 shadow-md darkMode">{{ el.item }}</span>
+            </p>
           </div>
           
 
@@ -50,28 +52,40 @@
         Content: [
           {
             title: "MIND",
-            desc: '"혼자 부딪혀봐야 온전히 내 것이 된다"',
-            desc2: "는 마인드를 가지고 있어, 혼자 찾아보고 고민하여 문제를 해결하는 것을 좋아합니다."
+            mind: [
+              { 
+                desc: '"Done is better than perfect."',
+                desc2: "너무 완벽을 추구하기 보다는 계속 보완하며 더 나은 방향으로 발전해야 한다는 마인드를 가지고 있습니다."
+              },
+              {
+                desc: '"Simple is best."',
+                desc2: "다른 사람들이 읽기 쉽고 유지보수성이 좋은 코드를 지향합니다."
+              }
+            ]
           },
           {
-            title: "EDUCATION",
+            title: "EXPERIENCE",
             date: [
-              "2022.12.29 ~ 2023.06.12",
-              "2023.01.01 ~ 2023.01.01"
+              "2022.12 ~ 2023.06",
+              "2014.03 ~ 2022.08",
             ],
             dateDesc:[
               "(디지털컨버전스) 스마트혼합-뷰(Vue) 활용 프론트엔드 개발자 양성과정 B",
-              "XXXX 내용 추가 입력"
+              "영남대학교 졸업",
             ]
           },
           {
-            title: "LICENSE",
-            date: [
-              "2022.08", "2022.07", "2022.06"
+            title: "LIKE",
+            like: [
+              {
+                item: "아이템1",
+                itemDesc: "아이템설명"
+              },
+              {
+                item: "아이템1",
+                itemDesc: "아이템설명"
+              },
             ],
-            dateDesc: [
-              "자격증1", "자격증2", "자격증3"
-            ]
           }
         ]
       }
