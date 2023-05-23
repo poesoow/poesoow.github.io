03a8500d100d8a193f0cf165097cc883d7c6076b
peosoow <ws71144712@daum.net>
2023-05-23 20:55:27 +0900

    목업 이미지 삭제

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -52,30 +52,24 @@
       <!-- /portfolio 주소에서 모든 worklist -->
       <div v-else>
         <h3>all</h3>
-        <div v-for="e in CateItem" :key="e" class="bg-white mb-8 pt-12 px-8 pb-16 rounded-md flex border flex-wrap darkMode group">
-          <div class="basis-[48%] relative group-even:order-1 xl:group-even:order-2 mokup-img">
-            <template v-if="e.responsive" >
-              <div v-for="e in 3" :key="e">
-                <img class="w-full" :src="require(`@/assets/images/mokup-${e}.png`)" alt="mokup" />
-              </div>
-            </template>
-            <template v-else>
-              <div>
-                <img :src="require(`@/assets/images/mokup-1.png`)" alt="mokup">
-              </div>
-            </template>
+        <div v-for="e in CateItem" :key="e" class="bg-white mb-8 pt-5 px-8 pb-5 rounded-md flex border flex-wrap darkMode group">
+          <div class="basis-[50%] relative group-even:order-1 xl:group-even:order-2 mokup-img">
+            <div class="border h-full">
+              <div class="w-full h-full"></div>
+            </div>
           </div>
-          <div class="basis-full md:basis-[52%] pt-10  group-even:order-2 xl:group-even:order-1">
-            <h3 class="text-2xl font-bold py-[10px] lg:pl-[50px]"> {{ e.descTitle }}</h3>
-            <p class="text-base pt-[10px] lg:pl-[50px]">{{ e.desc }}</p>
-            <p class="text-base pt-[10px] lg:pl-[50px]">키워드 : <span v-for="el in e.keyword" :key="el" class="mr-2">{{ el }}</span></p>
-            <p class="text-base pt-[10px] lg:pl-[50px]">사용툴 : <span v-for="el in e.tools" :key="el" class="mr-2 align-middle">{{ el }}</span></p>
-            <p class="text-base pt-[10px] lg:pl-[50px]">작업기간 : {{ e.date }}</p>
-            <p class="text-base pt-[10px] lg:pl-[50px]">기여도 : {{ e.contribution }}</p>
-            <p class="text-base pt-[10px] lg:pl-[50px]">
+          <div class="basis-full md:basis-[50%] pt-4  group-even:order-2 xl:group-even:order-1 xl:group-even:text-right lg:px-[50px] text-base">
+            <h3 class="text-2xl font-bold lg:pl-[50px]"> {{ e.descTitle }}</h3>
+            <p class="pt-[10px]">{{ e.desc }}</p>
+            <p class="pt-[10px]">키워드 : <span v-for="el in e.keyword" :key="el" class="mr-2">{{ el }}</span></p>
+            <p class="pt-[10px]">사용툴 : <span v-for="el in e.tools" :key="el" class="mr-2 align-middle">{{ el }}</span></p>
+            <p class="pt-[10px]">
+              <span>작업기간 : {{ e.date }} </span> <span>{{ e.date2 }}</span>
+            </p>
+            <p class="pt-[10px]">
               <ul>
-                <li>링크 : {{ e.link }}</li>
-                <li v-if="e.type == 'Publishing'">링크 : {{ e.originlink }}</li>
+                <li>사이트 : {{ e.link }}</li>
+                <li v-if="e.type == 'Publishing'">original 사이트 : {{ e.originlink }}</li>
               </ul> 
             </p>
           </div>
@@ -139,36 +133,23 @@ export default {
 </script>
 
 <style>
-.mokup-img > div{position: absolute; overflow: hidden; bottom: -35px;}
+.mokup-img > div{position: absolute; overflow: hidden; bottom: 0px;}
 /* 목업 pc버전 */
 .mokup-img > div:nth-child(1){width: 85%; left: 50%; transform: translateX(-50%);}
-.mokup-img > div:nth-child(1)::after{content: ''; position: absolute; width: 92%; height: 63%; background: url("../assets/images/preview.jpg") center top no-repeat; left: 19px; top: 20px; background-size: cover; transition: 5s;}
-/* 목업 태블릿버전 */
-.mokup-img > div:nth-child(2){width: 35%; right: 0;}
-.mokup-img > div:nth-child(2)::after{content: ''; position: absolute; width: 85%; height: 83%; background: url("../assets/images/preview.jpg") center top no-repeat; left: 17px; top: 24px; background-size: cover; transition: 5s;}
-/* 목업 모바일버전 */
-.mokup-img > div:nth-child(3){width: 23%; left: 0;}
-.mokup-img > div:nth-child(3)::after{content: ''; position: absolute; width: 90%; height: 90.5%; background: url("../assets/images/preview.jpg") center top no-repeat; left: 7px; top: 19px; border-radius: 5px; background-size: cover; transition: 5s;}
+.mokup-img > div:nth-child(1)::after{content: ''; position: absolute; width: 92%; height: 100%; background: url("../assets/images/preview.jpg") center top no-repeat; left: 19px; top: 0px; background-size: cover; transition: 5s;}
 /* 호버시 이미지 슬라이딩 */
 .mokup-img > div:hover::after{background-position: center bottom;}
 
 /* 태블릿버전 */
-@media screen and (max-width: 1200px) {
-  .mokup-img{flex-basis: 100%; height: 500px;}
-  .mokup-img > div:nth-child(1){width: 550px;}
-  .mokup-img > div:nth-child(2){width: 184px; right: 0;}
-  .mokup-img > div:nth-child(3){width: 126px; left: 0;}
-  .mokup-img > div:nth-child(1)::after{top: 23px; height: 62%; left: 21px; width: 93%;}
-  .mokup-img > div:nth-child(2)::after{width: 85%; top: 24px;}
+@media screen and (max-width: 1280px) {
+  .mokup-img{flex-basis: 100%; height: 380px;}
+  .mokup-img > div:nth-child(1){width: 100%;}
+  .mokup-img > div:nth-child(1)::after{top: 23px; height: 100%; left: 21px; width: 93%;}
 }
 /* 모바일버전 */
 @media screen and (max-width: 640px) {
   .mokup-img{height: 70.3125vw;}
   .mokup-img > div:nth-child(1){width: 100%;}
-  .mokup-img > div:nth-child(2){width: 35%;}
-  .mokup-img > div:nth-child(3){width: 20%;}
   .mokup-img > div:nth-child(1)::after{width: 92.5%; left: 3.8%; top: 4.7%;}
-  .mokup-img > div:nth-child(2)::after{left: 8%; top: 8%;}
-  .mokup-img > div:nth-child(3)::after{height: 92.0%; left: 5%; top: 5.5%;}
 }
 </style>
