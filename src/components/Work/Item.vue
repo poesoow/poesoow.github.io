bbf17ae19aeffdd3e64d2cc09bfed0981d6260a9
= <ws71144612@daum.net>
2023-05-29 16:54:41 +0900

    work 수정

--- src/components/Work/Item.vue
+++ src/components/Work/Item.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="basis-[50%] relative group-even:order-1 xl:group-even:order-2 mokup-img">
     <div class="border h-full">
-      <div class="w-full h-full"></div>
+      <div class="w-full h-full">
+        <img :src="item.img" :alt="item.title">
+      </div>
     </div>
   </div>
   <div class="basis-full md:basis-[50%] pt-4  group-even:order-2 xl:group-even:order-1 xl:group-even:text-right lg:px-[50px] text-base">
     <h3 class="text-2xl font-bold">
       <div>{{ item.title }} </div>
-      <div class="text-base">
-        <div>작업기간</div>
-        <div><span>{{ item.date2 }} </span> <span>[{{ item.date }}]</span></div>
-      </div>
+      <p class="text-base">{{ item.desc }}</p>
     </h3>
-    <p class="pt-[10px]">{{ item.desc }}</p>
-    <p class="pt-[10px]">
+    <div v-if="item.date2" class="pt-[10px]">
+      <div>작업기간</div>
+      <div><span>{{ item.date2 }} </span> <span v-if="item.date">[{{ item.date }}]</span></div>
+    </div>
+    <p v-if="item.features" class="pt-[10px]">
       주요특징
       <ul>
-        <li v-for="feature in item.features" :key="feature">{{ feature }}</li>
+        <li v-for="(feature, index) in item.features" :key="feature"><span>{{ index + 1 }}.</span> {{ feature }}</li>
       </ul>
     </p>
-    <p class="pt-[10px]">사용툴 : <span v-for="tool in item.tools" :key="tool" class="mr-2">{{ tool }}</span></p>
+    <p v-if="item.tools" class="pt-[10px]">사용툴 : <span v-for="tool in item.tools" :key="tool" class="mr-2">{{ tool }}</span></p>
     <template v-if="item.workers.team">
       <div class="pt-[10px]">팀프로젝트</div>
       <div class="pt-[5px]">
@@ -29,7 +31,6 @@
         </span>
       </div>
     </template>
-    <p v-else>개인프로젝트</p>
     <p class="pt-[10px]">
       <ul class="flex gap-2 justify-end">
         <li><a :href="item.links[0].link">보러가기</a></li>
@@ -52,7 +53,7 @@
 .mokup-img > div{position: absolute; overflow: hidden; bottom: 0px;}
 /* 목업 pc버전 */
 .mokup-img > div:nth-child(1){width: 85%; left: 50%; transform: translateX(-50%);}
-.mokup-img > div:nth-child(1)::after{content: ''; position: absolute; width: 92%; height: 100%; background: url("../../assets/images/preview.jpg") center top no-repeat; left: 19px; top: 0px; background-size: cover; transition: 5s;}
+/* .mokup-img > div:nth-child(1)::after{content: ''; position: absolute; width: 92%; height: 100%; background: url("../../assets/images/preview.jpg") center top no-repeat; left: 19px; top: 0px; background-size: cover; transition: 5s;} */
 /* 호버시 이미지 슬라이딩 */
 .mokup-img > div:hover::after{background-position: center bottom;}
 
