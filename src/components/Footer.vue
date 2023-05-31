88df6086ff66433f30b422c6a44409529db08569
peosoow <ws71144712@daum.net>
2023-05-31 10:16:33 +0900

    [style] 푸터 수정

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="max-w-7xl mx-auto bg-white darkMode py-7 text-center flex justify-between">
-    <p>Copyright 2023. SongWooSeop. All Right Reserved.</p>
-    <div class="">
-      <div class="mx-auto w-fit text-left">
-        <p class="">최근 수정일 : {{ modiList[0].date }}</p>
-        <p class="">작업 내용 : {{ modiList[0].desc }}
-        </p>
+  <div class="bg-white">
+    <!-- flex-col md:flex md:justify-between -->
+    <div class="max-w-7xl mx-auto bg-white darkMode py-7 px-[2%] text-center md:flex md:flex-row md:justify-between ">
+      <p>Copyright 2023. SongWooSeop. All Right Reserved.</p>
+      <div class="">
+        <div class="mx-auto w-fit text-left">
+          <p class="">최근 수정일 : {{ modiList[0].date }}</p>
+          <p class="">작업 내용 : {{ modiList[0].desc }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
