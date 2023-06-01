4b7941bd2793fbd99a2d1821cf1e69e1e91e8328
JeonMin-A <jeonminah13@gmail.com>
2023-06-01 19:35:36 +0900

    main watch 영역 글자 커지도록 수정

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -30,9 +30,9 @@
       <Contact />
     </div>
 
-    <div class="fixed top-40 right-12 hidden xl:block dark:text-[#d9d9d9] dark:bg-[#333]">
-     <ul class="flex flex-col gap-4">
-        <li v-for="(nav, index) in Nav" :key="nav" class="relative" :class="watchSection == index && 'text-[#009688] border-b-2 border-[#009688] w-fit'">
+    <div class="fixed top-40 right-4 hidden xl:block dark:text-[#d9d9d9] dark:bg-[#333]">
+     <ul class="flex flex-col gap-4 w-24">
+        <li v-for="(nav, index) in Nav" :key="nav" class="relative" :class="watchSection == index && 'text-[#009688] border-b-2 border-[#009688] w-fit text-2xl'">
           <button @click="SectionMove(`section-${(index + 1)}`)">{{ nav }}</button>
         </li>
       </ul>
