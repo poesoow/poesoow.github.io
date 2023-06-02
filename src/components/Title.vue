a51410884213470fd1c8efc0aaf26a86555e520f
= <ws71144612@daum.net>
2023-06-02 23:53:19 +0900

    title 컴포넌트 style 수정

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-7xl mt-5 p-5 mx-auto bg-white rounded-md border darkMode">
-    <h3 class="relative after:absolute mx-auto w-max after:w-[80%] after:h-[2px] after:bg-black after:dark:bg-[#d9d9d9] after:-bottom-1 after:left-2/4 after:-translate-x-2/4 tracking-widest text-center text-xl sm:text-2xl lg:text-3xl">
+    <h3 class="relative after:absolute mx-auto w-max text-[#009688] tracking-widest text-center text-xl sm:text-2xl lg:text-3xl">
       <slot></slot>
     </h3>
   </div>
