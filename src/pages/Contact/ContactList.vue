02b8f1dcb3734dcdd00c0eebe46baa71cee719d4
poesoow <ws71144712@daum.net>
2023-06-02 13:20:17 +0900

    router push로 수정

--- src/pages/Contact/ContactList.vue
+++ src/pages/Contact/ContactList.vue
@@ -20,15 +20,6 @@
      
       <ul class="">
         <li v-for="(data, index) in dataList" :key="index" class="even:bg-gray-50 ">
-          <!-- <router-link :to="{ name: 'ContactRead', query: { docId: dataIds[index] } }">
-            <div class="flex justify-between border-b text-center px-4 py-2 text-xs sm:text-sm hover:text-green-500">
-              <span class="basis-2/12 shrink-0">{{ dataList.length - index }}</span>
-              <span class="basis-5/12 shrink-0 text-left">{{ data.secret === false ? data.title : '비밀글 입니다.' }}</span>
-              <span class="basis-1/12 shrink-0">{{ data.secret === false ? data.name : '비밀작성자' }}</span>
-              <span class="basis-1/12 shrink-0">{{ BoardDate(index) }}</span>
-              <span class="basis-1/12 shrink-0">{{ data.complete ? '답변완료' : '답변대기' }}</span>
-            </div>
-          </router-link> -->
           <div @click="chkAndViewData(data.secret, dataIds[index], data.password)">
             <div class="flex justify-between border-b text-center px-4 py-2 text-xs sm:text-sm hover:text-green-500">
               <span class="basis-2/12 shrink-0">{{ dataList.length - index }}</span>
@@ -84,10 +75,10 @@
           if(inputPwd != password){
             alert('비밀번호를 다시 입력해주세요')
           } else {
-            this.$router.replace({ name: 'ContactRead', query: { docId: id } })
+            this.$router.push({ name: 'ContactRead', query: { docId: id } })
           }
         } else {
-          this.$router.replace({ name: 'ContactRead', query: { docId: id }})
+          this.$router.push({ name: 'ContactRead', query: { docId: id }})
         }
 
       }
