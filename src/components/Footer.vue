a0faa3b99a2c3af87fcdacec0c3fcb6a71400e77
= <ws71144612@daum.net>
2023-06-16 14:02:14 +0900

    깃허브 api 연결

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,8 @@
       <p>Copyright 2023. SongWooSeop. All Right Reserved.</p>
       <div class="">
         <div class="mx-auto w-fit text-left">
-          <p class="">최근 수정일 : {{ modiList[0].date }}</p>
-          <p class="">작업 내용 : {{ modiList[0].desc }}
-          </p>
+          <p class="">최근 수정일 : {{ formatDate(latestCommitdate) }}</p>
+          <p class="">작업 내용 : {{ latestCommitMessage }}</p>
         </div>
       </div>
     </div>
@@ -15,16 +14,44 @@
 
 <script>
   import modiList from '@/assets/modifylist.json'
+  import { Octokit } from 'octokit'
 
   export default {
     name: "FooterComponent",
     data() {
       return {
-        // 지금은 직접 json 파일에 작업한 내용을 적지만 나중에는 깃허브 커밋내역을 불러오기
-        // https://v3-docs.vuejs-korea.org/examples/#fetching-data
-        modiList: modiList
+        modiList: modiList,
+        latestCommit: null,
+        latestCommitMessage: '',
+        latestCommitdate: '',
       }
     },
+    mounted() {
+      this.fetchData()
+    },
+    methods: {
+      async fetchData() {
+        const octokit = new Octokit({
+          auth: process.env.VUE_APP_AUTHKEY,
+        })
+        await octokit.request('GET /repos/poesoow/poesoow.github.io/commits?per_page=1sha=main', {
+          owner: 'poesoow',
+          repo: 'poesoow.github.io',
+          ref: 'main',
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        }).then((res) => {
+          this.latestCommit = res.data[0].commit
+          this.latestCommitMessage = this.latestCommit.message
+          this.latestCommitdate = this.latestCommit.author.date
+        })
+      },
+      formatDate(date) {
+        // return date.replace(/T|Z/g, ' ')
+        return date.slice(0, 10)
+      }
+    }
   }
 </script>
 
